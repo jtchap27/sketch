eeb9ed80678772725f7c420b80eed7fc76be8487
jtchap27 <josh.chap2@gmail.com>
2023-11-27 21:11:19 -0500

    Complete project with random color boxes
    The project is complete and functional up through the random color generator.

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
 const container = document.querySelector(".container")
-let n = parseInt(prompt("Enter a whole number between 1 and 32"));
 
-for (let i = 1; i <= (n * n); i++) {
-  if (n > 32) {
-    break;
-  } else {
-  let singleBox = document.createElement("div");
-  singleBox.classList.add("singleBox");
-  singleBox.style.height = `${100 / n}%`;
-  singleBox.style.width = `${100 / n}%`;
-  container.appendChild(singleBox);
-  }
-};
+const newGridBtn = document.querySelector("#newGridBtn");
+newGridBtn.addEventListener("click", () => {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  };
+  let n = parseInt(prompt("Enter a whole number between 1 and 64"));
+  for (let i = 1; i <= (n * n); i++) {
+    if (n > 64) {
+      break;
+    } else {
+    let singleBox = document.createElement("div");
+    singleBox.classList.add("singleBox");
+    singleBox.style.height = `${100 / n}%`;
+    singleBox.style.width = `${100 / n}%`;
+    container.appendChild(singleBox);
+    }
+  };
+});
 
-let hoverBoxList = document.querySelectorAll(".singleBox");
-for (const hoverBox of hoverBoxList) {
-  hoverBox.addEventListener("mouseover", () => {
-    hoverBox.style.backgroundColor = "blue";
-  });
-};
+function randomInteger(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const hoverEffectBtnOn = document.querySelector("#hoverEffectBtnOn");
+
+hoverEffectBtnOn.addEventListener("click", function hoverEffectBtnOn() {
+  let hoverBoxList = document.querySelectorAll(".singleBox");
+  for (const hoverBox of hoverBoxList) {
+    hoverBox.addEventListener("mouseover", function randomRgbBackgroundColor() {
+      hoverBox.style.backgroundColor = `rgb(${randomInteger(255)},${randomInteger(255)},${randomInteger(255)})`;
+    });
+  };
+});
+
+const hoverEffectBtnOff = document.querySelector("#hoverEffectBtnOff");
+
+hoverEffectBtnOff.addEventListener("click", () => {
+  let hoverBoxList = document.querySelectorAll(".singleBox");
+  for (const hoverBox of hoverBoxList) {
+    hoverBox.style.backgroundColor = "white";
+    hoverBox.addEventListener("mouseover", function whiteBackgroundColor() {
+      hoverBox.style.backgroundColor = "white";
+    });
+  };
+});
